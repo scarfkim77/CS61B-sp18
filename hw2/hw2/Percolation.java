3ffd2b8b789afe4ed22b7383ace57c0aa59c09bc
77 <ckq.0928@163.com>
2020-08-29 15:42:01 +0800

    added check of exsitence of adjacent sites in method open

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,7 +4,7 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-    int rowOrCol;
+    int rowOrCol, size, numberOfOpenSites;
     int[] grid;
     WeightedQuickUnionUF gridUF;
     int virtualTopSite, virtualBottomSite;
@@ -15,33 +15,49 @@ public class Percolation {
             throw new IllegalArgumentException("N must be greater than 0!");
         }
         rowOrCol = N;
-        grid = new int[rowOrCol*rowOrCol];
-        for (int i = 0; i < rowOrCol * rowOrCol; i++) {
+        size = rowOrCol * rowOrCol;
+        numberOfOpenSites = 0;
+        grid = new int[size];
+        for (int i = 0; i < size; i++) {
             grid[i] = -1;
         }
         //多初始化2格，分别留给virtualTopSite和virtualBottomSite
-        gridUF = new WeightedQuickUnionUF(N*N+2);
-        virtualTopSite = N * N;
-        virtualBottomSite = N * N + 1;
+        gridUF = new WeightedQuickUnionUF(size+2);
+        virtualTopSite = size;
+        virtualBottomSite = size + 1;
+    }
+
+    //Change the row and col into 1D-index
+    public int getIndex(int row, int col) {
+        return row * rowOrCol + col;
+    }
+
+    //Check if the site is legal or not
+    public boolean isValid(int row, int col) {
+        if (row < 0 || row >= rowOrCol || col < 0 || col >= rowOrCol) {
+            return false;
+        }
+        return true;
     }
 
     //Open the site(row, col) if it is not open already
     public void open(int row, int col) {
-        int index = row * rowOrCol + col;
+        int index = getIndex(row, col);
         if (!isOpen(row, col)) {
             grid[index] = index;
+            numberOfOpenSites++;
         }
         try {
-            if (isOpen(row, col - 1)) {
+            if (isValid(row, col - 1) && isOpen(row, col - 1)) {
                 gridUF.union(index, index - 1);
             }
-            if (isOpen(row, col + 1)) {
+            if (isValid(row, col + 1) && isOpen(row, col + 1)) {
                 gridUF.union(index, index + 1);
             }
-            if (isOpen(row - 1, col)) {
+            if (isValid(row - 1, col) && isOpen(row - 1, col)) {
                 gridUF.union(index, index - rowOrCol);
             }
-            if (isOpen(row + 1, col)) {
+            if (isValid(row + 1, col) && isOpen(row + 1, col)) {
                 gridUF.union(index, index + rowOrCol);
             }
             if (row == 0) {
@@ -56,17 +72,17 @@ public class Percolation {
 
     //Is the site(row, col) open?
     public boolean isOpen(int row, int col) {
-        if (row < 0 || row >= rowOrCol || col < 0 || col >= rowOrCol) {
+        if (!isValid(row, col)) {
             throw new IndexOutOfBoundsException("The given site is not in the grid!");
         }
-        int index = row * rowOrCol + col;
+        int index = getIndex(row, col);
         return grid[index] != -1;
     }
 
     //Is the site(row, col) full?
     public boolean isFull(int row, int col) {
         if (isOpen(row, col)) {
-            int index = row * rowOrCol + col;
+            int index = getIndex(row, col);
             return gridUF.connected(virtualTopSite, index);
         }
         return false;
@@ -74,13 +90,7 @@ public class Percolation {
 
     //Number of open sites
     public int numberOfOpenSites() {
-        int num = 0;
-        for (int i = 0; i < rowOrCol * rowOrCol; i++) {
-                if (grid[i] != -1) {
-                    num++;
-            }
-        }
-        return num;
+        return numberOfOpenSites;
     }
 
     //Does the system percolates?
@@ -91,14 +101,11 @@ public class Percolation {
     // use for unit testing (not required)
     public static void main(String[] args) {
         Percolation test = new Percolation(3);
-        System.out.println(test.rowOrCol);
         test.open(0, 2);
-        /*test.open(0, 2);
         test.open(1, 2);
         test.open(2, 2);
         test.open(2, 0);
-        System.out.println(test.percolates());
-        System.out.println(test.isFull(2, 0));
-        */
+        //System.out.println(test.percolates());
+        System.out.println(test.isFull(0, 2));
     }
 }
